5f92a69cc8b9dda12449b1508ae2c4abecf179a3
Leonardo Scarlato <leonardo.scarlato@hotmail.com>
2024-05-26 19:25:19 -0300

    att login

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import Snackbar from '@mui/material/Snackbar';
-import Alert from '@mui/material/Alert';
+import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
 import "./Login.css";
 import icon from "./assets/favicon.ico";
 
@@ -9,10 +9,10 @@ function LoginPage() {
   const navigate = useNavigate();
   const [message, setMessage] = useState("");
   const [open, setOpen] = useState(false);
-  const [alertSeverity, setAlertSeverity] = useState('success'); // 'error' ou 'success'
+  const [alertSeverity, setAlertSeverity] = useState("success"); // 'error' ou 'success'
 
   const handleClose = (reason) => {
-    if (reason === 'clickaway') {
+    if (reason === "clickaway") {
       return;
     }
     setOpen(false);
@@ -20,45 +20,45 @@ function LoginPage() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
-    const email = formData.get('email');
-    const password = formData.get('password');
-  
+    const email = formData.get("email");
+    const password = formData.get("password");
+
     try {
-      const response = await fetch('https://api.pontochave.projetohorizontes.com/user/token', {
-        method: 'POST',
+      const response = await fetch("http://127.0.0.1:8000/user/token", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
+          "Content-Type": "application/x-www-form-urlencoded",
         },
-        body: `username=${email}&password=${password}`
+        body: `username=${email}&password=${password}`,
       });
-  
+
       if (!response.ok) {
-        throw new Error('Credenciais inválidas!');
+        throw new Error("Credenciais inválidas!");
       }
-  
+
       const data = await response.json();
-      localStorage.setItem('token', data.access_token);
-      setMessage("Login bem-sucedido!");
-      setAlertSeverity('success');  
-      setOpen(true);  
-  
-      fetch("https://api.pontochave.projetohorizontes.com/user/me", {
+      localStorage.setItem("token", data.access_token);
+      setMessage("Login bem-sucedido! Redirecionando...");
+      setAlertSeverity("success");
+      setOpen(true);
+
+      fetch("http://127.0.0.1:8000/user/me", {
         method: "GET",
         headers: {
-          'Authorization': `Bearer ${data.access_token}`
-        }
+          Authorization: `Bearer ${data.access_token}`,
+        },
       })
-      .then((response) => response.json())
-      .then((data) => {
-        localStorage.setItem('user_id', JSON.stringify(data.id));
-        localStorage.setItem('name', data.name);
-      })
-  
-      setTimeout(() => navigate("/home"), 2000);  
+        .then((response) => response.json())
+        .then((data) => {
+          localStorage.setItem("user_id", JSON.stringify(data.id));
+          localStorage.setItem("name", data.name);
+        });
+
+      setTimeout(() => navigate("/home"), 1000);
     } catch (error) {
       setMessage("Erro: " + error.message);
-      setAlertSeverity('error'); 
-      setOpen(true);  
+      setAlertSeverity("error");
+      setOpen(true);
     }
   };
 
@@ -72,15 +72,21 @@ function LoginPage() {
       <div className="App-login" id="login">
         <h1>Fazer login</h1>
         <form onSubmit={handleSubmit}>
-          <input name="email" type="email" placeholder="E-mail" required />
-          <input name="password" type="password" placeholder="Senha" required />
+          <label htmlFor="email">E-mail</label>
+          <input name="email" type="email" placeholder="Insira seu e-mail..." required />
+          <label htmlFor="password">Senha</label>
+          <input name="password" type="password" placeholder="Insira sua senha..." required />
           <button type="submit">Entrar</button>
         </form>
         <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-          <Alert onClose={handleClose} severity={alertSeverity} sx={{ width: '100%' }}>
+          <Alert
+            onClose={handleClose}
+            severity={alertSeverity}
+            sx={{ width: "100%" }}
+          >
             {message}
           </Alert>
-        </Snackbar>        
+        </Snackbar>
         <p>
           Não tem uma conta? <Link to="/signup">Cadastre-se</Link>
         </p>
