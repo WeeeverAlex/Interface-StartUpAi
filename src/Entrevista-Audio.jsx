ca4069a44c103a9055e23fa8cc48f13a2ccba882
WeeeverAlex <awever4000@gmail.com>
2024-05-28 09:25:27 -0300

    commt recording botton

--- src/Entrevista-Audio.jsx
+++ src/Entrevista-Audio.jsx
@@ -199,7 +199,7 @@ const Entrevista_Audio = () => {
             <div className="interview-answer">
               <div className="audio-answer">
                 <button onClick={isRecording ? stopRecording : startRecording} className="microfone-button">
-                  <img src={isRecording ? "Enviar Gravação" : microphoneIcon} alt="Microfone" />
+                  <img src={isRecording ? recordingIcon : microphoneIcon} alt="Microfone" />
                   {isRecording ? "" : ""}
                 </button>
               </div>
